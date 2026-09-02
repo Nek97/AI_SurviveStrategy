0eeaeccc8baf9af500b78246cdb11a415fbdaf71
AnielloTortora_VitaDev <aniello.tortora@vitameals.com>
2026-09-02 10:19:08 +0200

    fix: Docusaurus configuration

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://nek97.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/AI_SurviveStrategy/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Nek97', // Usually your GitHub org/user name.
+  projectName: 'AI_SurviveStrategy', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
